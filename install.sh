7c2d93100da287d86cfb1df8a7ee995550578d9f
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-04-20 11:04:14 +0545

    fixed a bug

--- install.sh
+++ install.sh
@@ -8,10 +8,13 @@ serviceFile="$serviceDirectory/muteSpotifyAds.service"
 
 mkdir -p "$scriptDirectory"
 cp "./muteSpotifyAds.sh" "$scriptFile"
+echo "copied script file to $scriptFile"
 mkdir -p "$serviceDirectory"
 echo -e "[Unit]\nDescription=Mute Spotify Ads\nAfter=graphical-session.target\n" > "$serviceFile"
 echo -e "[Service]\nType=forking\nExecStartPre=/bin/bash -c \"$scriptFile & disown \$!\"\nExecStart=/bin/true\nRestart=always\n" >> "$serviceFile"
-echo "[Install]\nWantedBy=graphical-session.target" >> "$serviceFile"
+echo -e "[Install]\nWantedBy=graphical-session.target" >> "$serviceFile"
+echo "created service file for user $username in $serviceFile"
 
 systemctl --user daemon-reload
 systemctl --user enable --now muteSpotifyAds.service
+echo "Installation successful"
