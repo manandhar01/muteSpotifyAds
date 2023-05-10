10ebef7fc960d1c735ece3fa627e3c31103e89db
Bibek Manandhar <manandhar.bibek01@gmail.com>
2023-05-10 14:05:06 +0545

    using only playerctl now

--- muteSpotifyAds.sh
+++ muteSpotifyAds.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/env bash
 
 mutedByScript=0
+initial_volume="$(playerctl -p spotify volume 2> /dev/null | echo 1)"
 while true
 do
 	if pgrep -x "spotify" > /dev/null; then
-		status="$(playerctl --player=spotify metadata --format '{{status}}')"
+		status="$(playerctl -p spotify status)"
 		if [ "$status" = "Playing" ]; then
-			title="$(playerctl --player=spotify metadata --format '{{title}}')"
+			title="$(playerctl -p spotify metadata -f '{{title}}')"
 			if [ "$title" = "Advertisement" ]; then
-				muteStatus="$(pactl get-sink-mute @DEFAULT_SINK@)"
-				if [ "$muteStatus" = "Mute: no" ]; then
-					pactl set-sink-mute @DEFAULT_SINK@ toggle
+				if (( $(echo "`playerctl -p spotify volume` > 0" | bc -l) )); then
+					initial_volume="$(playerctl -p spotify volume)"
+					playerctl -p spotify volume 0
 					mutedByScript=1
 				fi
 			else
 				if [ $mutedByScript -eq 1 ]; then
-					pactl set-sink-mute @DEFAULT_SINK@ toggle
+					playerctl -p spotify volume $initial_volume
 					mutedByScript=0
 				fi
 			fi
