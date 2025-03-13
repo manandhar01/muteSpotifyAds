9299224138b4aac665bb7eac0af6ca4714d5fad7
Bibek Manandhar <manandhar.bibek01@gmail.com>
2025-03-13 11:30:18 +0545

    bug fix

--- muteSpotifyAds.sh
+++ muteSpotifyAds.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
 mutedByScript=0
-initial_volume="$(playerctl -p spotify volume 2> /dev/null | echo 1)"
-while true
-do
-	if pgrep -x "spotify" > /dev/null; then
+initial_volume="$(playerctl -p spotify volume 2>/dev/null | cat)"
+
+while true; do
+	if pgrep -x "spotify" >/dev/null; then
 		status="$(playerctl -p spotify status)"
 		if [ "$status" = "Playing" ]; then
 			title="$(playerctl -p spotify metadata -f '{{title}}')"
 			if [ "$title" = "Advertisement" ]; then
-				if (( $(echo "`playerctl -p spotify volume` > 0" | bc -l) )); then
+				if (($(echo "$(playerctl -p spotify volume) > 0" | bc -l))); then
 					initial_volume="$(playerctl -p spotify volume)"
 					playerctl -p spotify volume 0
 					mutedByScript=1
 				fi
 			else
 				if [ $mutedByScript -eq 1 ]; then
-					playerctl -p spotify volume $initial_volume
+					playerctl -p spotify volume "$initial_volume"
 					mutedByScript=0
 				fi
 			fi
