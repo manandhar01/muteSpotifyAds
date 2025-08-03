be573473add53ee64d8763b59b3b52d95e71f4d4
Bibek Manandhar <manandhar.bibek01@gmail.com>
2025-08-03 17:16:41 +0545

    install script updated

--- install.sh
+++ install.sh
@@ -1,20 +1,72 @@
 #!/usr/bin/env bash
 
-username="$(whoami)"
-scriptDirectory="/home/$username/bin"
-scriptFile="$scriptDirectory/muteSpotifyAds.sh"
-serviceDirectory="/home/$username/.config/systemd/user"
-serviceFile="$serviceDirectory/muteSpotifyAds.service"
+RED=$'\033[0;31m'
+GREEN=$'\033[0;32m'
+YELLOW=$'\033[1;33m'
+RESET=$'\033[0m'
 
-mkdir -p "$scriptDirectory"
-cp "./muteSpotifyAds.sh" "$scriptFile"
-echo "copied script file to $scriptFile"
-mkdir -p "$serviceDirectory"
-echo -e "[Unit]\nDescription=Mute Spotify Ads\nAfter=graphical-session.target\n" > "$serviceFile"
-echo -e "[Service]\nType=simple\nExecStart=/bin/bash  $scriptFile\nRestart=always\n" >> "$serviceFile"
-echo -e "[Install]\nWantedBy=default.target" >> "$serviceFile"
-echo "created service file for user $username in $serviceFile"
+script_file="$HOME/bin/mute_spotify_ads.sh"
+service_file="$HOME/.config/systemd/user/mute-spotify-ads.service"
 
-systemctl --user daemon-reload
-systemctl --user enable --now muteSpotifyAds.service
-echo "Installation successful"
+function install() {
+    printf "\n%s🛠️ Installing...%s\n\n" "$GREEN" "$RESET"
+
+    mkdir -p "$(dirname "$script_file")"
+    mkdir -p "$(dirname "$service_file")"
+
+    cp "./mute_spotify_ads.sh" "$script_file"
+    printf "%s✅ Installed script: %s%s\n" "$GREEN" "$script_file" "$RESET"
+
+    cp "./mute-spotify-ads.service" "$service_file"
+    printf "%s✅ Installed service: %s%s\n" "$GREEN" "$service_file" "$RESET"
+
+    printf "%s🛠️ Enabling service...%s\n" "$GREEN" "$RESET"
+    systemctl --user daemon-reload
+
+    printf "%s" "$YELLOW"
+    systemctl --user enable --now mute-spotify-ads.service
+    printf "%s" "$RESET"
+
+    printf "\n%s💯 Installation Complete%s\n\n" "$GREEN" "$RESET"
+}
+
+function remove() {
+    printf "\n%s🚮 Removing...%s\n\n" "$RED" "$RESET"
+
+    printf "%s🛠️ Disabling service...%s\n" "$GREEN" "$RESET"
+
+    printf "%s" "$YELLOW"
+    systemctl --user disable --now mute-spotify-ads.service
+    printf "%s" "$RESET"
+
+    systemctl --user daemon-reload
+
+    rm "$service_file"
+    printf "%s🗑️ Deleted service: %s%s\n" "$RED" "$service_file" "$RESET"
+
+    rm "$script_file"
+    printf "%s🗑️ Deleted script: %s%s\n" "$RED" "$script_file" "$RESET"
+
+    printf "\n%s💯 Removal Complete%s\n\n" "$RED" "$RESET"
+}
+
+printf "\n%s👉 Please select an operation:%s\n" "$GREEN" "$RESET"
+printf " 1) 🛠️ Install\n"
+printf " 2) 🗑️ Remove\n"
+printf " 0) ❌ Exit\n\n"
+
+read -rp "${YELLOW}🔢 Enter a number: ${RESET}" operation
+
+if [[ "$operation" -eq 0 ]]; then
+    printf "\n%s🚶🏼‍♂️ Exiting%s\n\n" "$RED" "$RESET"
+    exit 0
+elif [[ "$operation" -ne 1 && "$operation" -ne 2 ]]; then
+    printf "\n%s❗ Invalid input. 🚶🏼‍♂️ Exiting.%s\n\n" "$RED" "$RESET" >&2
+    exit 1
+fi
+
+if [ "$operation" -eq 1 ]; then
+    install
+else
+    remove
+fi
